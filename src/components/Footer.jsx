1b8433fc0c1f63949bdef25c68a0ea36341497d6
zekud <zekudk@gmail.com>
2024-03-17 17:56:20 +0300

    Add images and update Contact and Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/Logo-white.svg";
+import facebook from "../assets/images/g10.png";
+import twitter from "../assets/images/Logo_1_.png";
+import instagram from "../assets/images/Clip path group.png";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="flex flex-col items-center gap-10 p-10 w-full md:flex-row md:justify-between bg-accentRed-dark md:items-center">
+      <div className="flex flex-col md:flex-row text-center md:text-start gap-4 text-lightGray items-center">
+        {/* logo */}
+        <span>
+          <img src={logo} alt="logo" />
+        </span>
+        {/* description */}
+        <p style={{ maxWidth: "200px" }} className="text-lightGray text-xs">
+          Step into a world of beauty and elegance. Your journey to stunning
+          nails begins here.Nailed It crafting perfection, one nail at a time.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4 text-center">
+        {/* title */}
+        <p className="text-white text-sm font-bold">Follow us</p>
+        {/* links */}
+        <div className="flex gap-2 ">
+          <a href="#">
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a href="#">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="#">
+            <img src={instagram} alt="instagram" />
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 items-center md:items-start">
+        {/* links */}
+        <p className="text-white text-sm font-bold">Help Center</p>
+        <a href="#" className="text-lightGray text-sm ">
+          Contact Us
+        </a>
+        <a href="#" className="text-lightGray text-sm ">
+          Terms and Conditions
+        </a>
+        <a href="#" className="text-lightGray text-sm ">
+          Privacy Policy
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
